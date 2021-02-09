6b7c94800e0e3b1709ce31336bf8501b2000a68f
Linda O'Hara <lindaaohara@gmail.com>
2021-02-08 19:47:45 -0500

    start project

---
+++ src/main/java/ohara/linda/springbootcrudhibernateexample/SpringbootCrudHibernateExampleApplication.java
@@ -0,0 +1,13 @@
+package ohara.linda.springbootcrudhibernateexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudHibernateExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudHibernateExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ohara/linda/springbootcrudhibernateexample/SpringbootCrudHibernateExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ohara.linda.springbootcrudhibernateexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudHibernateExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
